9194fbbe07b4d7bf511e2c53cb1ba0f07eb82796
Vignesh Vittal Srinivasaragavan <vickyragav95@gmail.com>
2021-09-24 14:50:59 -0400

    Created a vector class with 3 components

--- src/pumiMBBL_utils.hpp
+++ src/pumiMBBL_utils.hpp
@@ -2,7 +2,53 @@
 #define pumiMBBL_utils_hpp
 
 namespace pumi{
+/**
+ * @brief Convenience class for 3-vectors of double.
+ */
+class Vector3 {
+public:
+    double components_[3];
 
+    KOKKOS_FUNCTION
+    Vector3();
+
+    /**
+     * @brief Default destructor.
+     */
+    ~Vector3() = default;
+
+    KOKKOS_FUNCTION
+    Vector3(double x1, double x2, double x3);
+
+    KOKKOS_FUNCTION
+    Vector3(double x[3]);
+};
+
+using Vector3View = Kokkos::View<Vector3*>;
+
+/**
+ * @brief Default constructor.
+ */
+KOKKOS_INLINE_FUNCTION
+Vector3::Vector3() : components_{0.0, 0.0, 0.0} {}
+
+/**
+ * @brief Construct a vector with three given components.
+ *
+ * @param[in] x1 x1-component of vector
+ * @param[in] x2 x2-component of vector
+ * @param[in] x3 x3-component of vector
+ */
+KOKKOS_INLINE_FUNCTION
+Vector3::Vector3(double x1, double x2, double x3) : components_{x1, x2, x3} {}
+
+/**
+ * @brief Construct a 3-vector from an array.
+ *
+ * @param[in] x double[3] array.
+ */
+KOKKOS_INLINE_FUNCTION
+Vector3::Vector3(double x[3]) : components_{x[0], x[1], x[2]} {}
 /**
  * @brief Copies a derived class to device and returns pointer to it.
  *
